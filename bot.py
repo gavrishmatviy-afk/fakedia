f5e7f4fe19d6a8263572ff980a88b237723901df
gavrishmatviy-afk <gavrishmatviy@gmail.com>
2026-02-19 19:48:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("7680399380:AAHYyu6BaTAx4v5ARFjcLl79fxvJRPHDp8Q")
+TOKEN = os.getenv("8368024318:AAEoV01O8LSQy4_IvTfQ6AmaqgUz19dA3cY")
 
 # --- Flask сервер (щоб Render бачив порт) ---
 app_flask = Flask(__name__)
